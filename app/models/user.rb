a162f870d0ca45089dc19b48eccec6653a2380d4
matthewxvoorhees <matthew.crnich@gmail.com>
2020-10-29 14:28:10 -0600

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
+
+  # def user_attributes=(user_attributes)
+  #   self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank?
+  # end
+
 end
